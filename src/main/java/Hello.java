c62ab504c83a2fd1192f1192aa183e8a746f2a98
Christophe Maldivi <christophe.maldivi@orange.com>
2018-09-20 23:05:13 +0200

    try to pass in idea and cmd line

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,12 @@
 public class Hello {
 
   public void callJava() {
+    // test java 10
+    // var test = 10;
+
+    // test java 9
+    // List list = List.of("titi");
+
     System.out.println("java says ah");
   }
 
